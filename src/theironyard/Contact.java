7318b35d36b74299efad710b28c46c6bee1b86e6
Alex <amhughes@g.cofc.edu>
2016-02-16 19:29:24 -0500

    uhh, writes file, and reads it correctly back into app, little ugly

--- src/theironyard/Contact.java
+++ src/theironyard/Contact.java
@@ -3,6 +3,10 @@
  */
 package theironyard;
 
+import javafx.collections.ObservableList;
+
+import java.util.ArrayList;
+
 public class Contact {
     String name;
     String phone;
@@ -14,8 +18,33 @@ public class Contact {
         this.email = email;
     }
 
+
     @Override
     public String toString() {
         return String.format("%s, %s, %s", name, phone, email);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
--- src/theironyard/Controller.java
+++ src/theironyard/Controller.java
@@ -5,15 +5,22 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import jodd.json.JsonParser;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 
 public class Controller implements Initializable {
 
     ObservableList<Contact> contacts = FXCollections.observableArrayList();//creates new arraylist observable thingy
 
-    @FXML       //connects it to interface
+    @FXML
     ListView list;
 
     @FXML
@@ -23,23 +30,63 @@ public class Controller implements Initializable {
     @FXML
     TextField textEmail;
 
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        try {
+            contacts = readFile();
+        }
+        catch (Exception e) {
+
+        }
         list.setItems(contacts);
     }
 
-    public void addContact() {
+    public void addContact() throws IOException {
         if (!textName.getText().isEmpty() && !textPhone.getText().isEmpty() && !textEmail.getText().isEmpty()) {
             contacts.add(new Contact(textName.getText(), textPhone.getText(), textEmail.getText()));
         }
         textName.setText("");
         textPhone.setText("");
         textEmail.setText("");
+        writeFile(contacts);
     }
 
-    public void removeContact() {
+    public void removeContact() throws IOException {
         Contact contact = (Contact) list.getSelectionModel().getSelectedItem();
         contacts.remove(contact);
+        writeFile(contacts);
+    }
+
+    public void writeFile(ObservableList<Contact> contacts) throws IOException {
+        File f = new File("contacts.txt");
+        FileWriter fw = new FileWriter(f);
+        ArrayList<Contact> listS = new ArrayList<>(contacts);
+        ArrayList<String> contactsStr = new ArrayList<>();
+        for (int i = 0; i < listS.size(); i++) {
+            contactsStr.add(listS.get(i).getName());
+            contactsStr.add(listS.get(i).getPhone());
+            contactsStr.add(listS.get(i).getEmail());
+        }
+        for (String info : contactsStr) {
+            fw.append(info + "\n");
+        }
+        fw.close();
+    }
+
+    public ObservableList<Contact> readFile() throws FileNotFoundException {
+        File f = new File("contacts.txt");
+        Scanner s = new Scanner(f);
+        ArrayList<String> contactsStr = new ArrayList<>();
+        while (s.hasNext()) {
+            String line = s.nextLine();
+            contactsStr.add(line);
+        }
+        ArrayList<Contact> tempContacts = new ArrayList<>();
+        for (int i = 0; i < contactsStr.size(); i+=3) {
+            tempContacts.add(new Contact(contactsStr.get(i), contactsStr.get(i+1), contactsStr.get(i+2)));
+        }
+        return FXCollections.observableArrayList(tempContacts);
     }
 
 }
